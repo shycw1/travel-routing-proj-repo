e24c2ee686378f8c21a54b70d9230918a418230f
Chunyu Wang <shycw1@student.ubc.ca>
2022-11-22 15:26:15 -0800

    loadded dataset

---
+++ map_reader.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+
+def read_tsp(filename):
+    with open(filename) as f:
+        node_coord_start = None
+        dimension = None
+        lines = f.readlines()
+        i = 0
+        while not dimension or not node_coord_start:
+            line = lines[i]
+            if line.startswith('DIMENSION :'):
+                dimension = int(line.split()[-1])
+            if line.startswith('NODE_COORD_SECTION'):
+                node_coord_start = i
+            i = i+1
+        print('Problem with {} cities read.'.format(dimension))
+
+        f.seek(0)
+
+        # Read a data frame out of the file descriptor
+        cities = pd.read_csv(
+            f,
+            skiprows=node_coord_start + 1,
+            sep=' ',
+            names=['city', 'y', 'x'],
+            dtype={'city': str, 'x': np.float64, 'y': np.float64},
+            header=None,
+            nrows=dimension
+        )
+
+        # cities.set_index('city', inplace=True)
+
+        return cities
