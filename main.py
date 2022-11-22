b057b948127dd80b026a2a7fb73cb366049467ff
Chunyu Wang <shycw1@student.ubc.ca>
2022-11-22 15:27:31 -0800

    update main.py

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
 from sys import argv
-
+from map_reader import read_tsp
 import numpy as np
+
+
+
+
+
+def main():
+    if len(argv) != 2:
+        print("Correct use: python src/main.py <filename>.tsp")
+        return -1
+    problem = read_tsp(argv[1])
+    print(problem.head(5))
+
+
+
+
+
+
+
+    
+
+main()
